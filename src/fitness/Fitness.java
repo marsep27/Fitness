b0459222ee53e1fe10859ceda4a6209bcc52874f
marsep27 <marsep27@hotmail.com>
2015-03-25 10:31:01 -0600

    Creación del proyecto

---
+++ src/fitness/Fitness.java
@@ -0,0 +1,12 @@
+package fitness;
+
+public class Fitness {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
